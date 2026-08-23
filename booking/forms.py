2451ca45fdf993918ca9c2925c96374a1609de0e
akshobhya <bgmiak2005@gmail.com>
2026-08-23 15:54:23 +0530

    Add RegisterForm for user registration

--- booking/forms.py
+++ booking/forms.py
@@ -19,3 +19,14 @@ class PassengerCountForm(forms.Form):
     )
 
 
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
