30a0a6fde5160dd8c43d7e2e743196ae5077c89f
akshobhya <bgmiak2005@gmail.com>
2026-08-23 15:52:01 +0530

    Update forms.py

--- booking/forms.py
+++ booking/forms.py
@@ -19,14 +19,3 @@ class PassengerCountForm(forms.Form):
     )
 
 
-class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
